aee12747f59715fb572207a25f55d437ed3f35ac
RishikDevTerminal <82426895+rishikdev@users.noreply.github.com>
2023-02-13 11:20:01 -0600

    UPDATED UserInfo

--- Assignments/UserInfo/UserInfo/ViewModels/UserViewModel.swift
+++ Assignments/UserInfo/UserInfo/ViewModels/UserViewModel.swift
@@ -11,7 +11,7 @@ class UserViewModel {
     var user: UserModel?
     
     func getUser(completion: @escaping () -> Void) {
-        NetworkManager.sharedInstance.getUserInfo(from: Constants.urls.userURL.rawValue) { [weak self] result in
+        NetworkManager.sharedInstance.getUserInfo(from: Constants.urls.userURL.rawValue) { [weak self] (result: Result<UserModel, Error>) in
             switch result {
             case .success(let user):
                 self?.user = user
