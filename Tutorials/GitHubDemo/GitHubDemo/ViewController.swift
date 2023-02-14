8eb73eb904aa41a8ae6f2c19a0d7a559adfeee93
RishikDevTerminal <82426895+rishikdev@users.noreply.github.com>
2023-02-14 09:27:21 -0600

    ADDED GitHubDemo Tutorial

---
+++ Tutorials/GitHubDemo/GitHubDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubDemo
+//
+//  Created by Rishik Dev on 14/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
